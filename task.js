279db719cb2a6173401fba3f6c47a4f91f6ed804
Severnyi-Lis <xnestro@mail.ru>
2024-01-09 22:35:02 +0300

    убрал js из body

---
+++ task.js
@@ -0,0 +1,42 @@
+function getDay(date) {
+    let day = date.getDay()
+    if(day == 0) day = 7
+    return day -1
+}
+    function createCalendar(elem, year, month) {
+     elem = document.querySelector(elem)
+
+    let mon = month - 1;
+    let d = new Date(year, mon)
+
+    let table = `
+    <caption>${month}.${year}</caption>
+    <table>
+        <tr>
+            <th>Пн</th>
+            <th>Вт</th>
+            <th>Ср</th>
+            <th>Чт</th>
+            <th>Пт</th>
+            <th>Сб</th>
+            <th>Вс</th>
+        </tr>
+        <tr>
+        `;
+
+    for(let i=0; i<getDay(d); i++){
+    table += '<td></td>'
+    }
+
+    while(d.getMonth() ==mon){
+        table += '<td>' + d.getDate() + '</td>'
+        if(getDay(d) % 7 ==6) {
+            table += '</tr><tr>'
+        }
+        d.setDate(d.getDate()+1)
+    }
+
+    table +='</tr></table>';
+    elem.innerHTML = table
+}
+
